e4deff3bfe8562c67dc59a3a058d45a8185b7fdb
amarnath2706 <amarnath.arjsd@gmail.com>
2024-12-17 13:58:06 +0530

    commit

--- main.py
+++ main.py
@@ -19,3 +19,15 @@ vertexai.init(project=project_id, location=project_region)
 
 #load the model
 model = GenerativeModel("gemini-1.0-pro")
+
+def user_interfaces():
+    st.set_page_config("VertexAI Demo")
+    st.header("Gemini-Pro Model On VertexAI")
+
+    user_question = st.text_input("Ask me anything......")
+
+    if user_question:
+        response = model.generate_content(user_question,stream=True)
+
+        for res in response:
+            st.write(res.text, end="")
